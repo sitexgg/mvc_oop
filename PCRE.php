07bc372b59d79e8dbda9593f886b72dc1f0a599d
qazaqlife <8gg001@gmail.com>
2018-12-17 04:03:00 +0600

    new Class PCre

--- PCRE.php
+++ PCRE.php
@@ -1,20 +1,34 @@
 <?php
 /*Regular Expressions Perl-Compatible*/
-/*Регулярные Выражение Perl-Совместмые*/
+
 
 class PCre {
 
 
+	public $Dmat = [];
+	public $Emat = [];
+
 	public function email($str) {
- 		echo preg_match('#^[a-zA-Z-.]+@[a-z]+\.[a-z]{2,4}$#', $str);
+ 		if(preg_match_all('#^([a-zA-Z0-9-.]+)@([a-z]+)\.([a-z]{2,4})$#', $str, $mat)) {
+ 			$this->Emat = $mat;
+		}
 	}
 
 	public function domain($str) {
-		echo preg_match_all('#[a-z]+\.([a-z]{2,4})#', 'domain.net');
+		if(preg_match_all('#^([a-z0-9]+)\.([a-z]{2,4}$)#', $str, $mat)) {
+			$this->Dmat = $mat;
+		}
 	}
 
+	
 
 
 }
 
+$e = '234fd234sdf@gsg.sew';
+$x = 'don.ss';
 $reg = new PCre;
+$reg->domain($x);
+echo '<pre>';
+
+var_dump($reg->Dmat);
