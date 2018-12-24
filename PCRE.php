8fb94288336c85c7d70a35ce3b015e1881c97be8
Yaroslav <8gg001@gmail.com>
2018-12-24 11:23:41 +0600

    123
    123

--- PCRE.php
+++ PCRE.php
@@ -2,33 +2,27 @@
 /*Regular Expressions Perl-Compatible*/
 
 
-class PCre {
+class PCre 
+{
 
 
 	public $Dmat = [];
 	public $Emat = [];
 
-	public function email($str) {
- 		if(preg_match_all('#^([a-zA-Z0-9-.]+)@([a-z]+)\.([a-z]{2,4})$#', $str, $mat)) {
+	public function email($str)
+	{
+ 		if(preg_match_all('#^([a-zA-Z0-9-.]+)@([a-z]+)\.([a-z]{2,4})$#', $str, $mat))
+		{
  			$this->Emat = $mat;
 		}
 	}
 
-	public function domain($str) {
-		if(preg_match_all('#^([a-z0-9]+)\.([a-z]{2,4}$)#', $str, $mat)) {
+	public function domain($str)
+	{
+		if(preg_match_all('#^([a-z0-9]+)\.([a-z]{2,4}$)#', $str, $mat))
+		{
 			$this->Dmat = $mat;
 		}
 	}
-
-	
-
-
 }
 
-$e = '234fd234sdf@gsg.sew';
-$x = 'don.ss';
-$reg = new PCre;
-$reg->domain($x);
-echo '<pre>';
-
-var_dump($reg->Dmat);
