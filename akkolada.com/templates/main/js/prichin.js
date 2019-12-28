4a801bd0fad3772820910805880a65a455bbaac1
webnetkz <8gg001@gmail.com>
2019-12-28 23:33:32 +0600

    update

--- akkolada.com/templates/main/js/prichin.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function(){
-	jQuery(".top_2 .prichin_title").click(function(){
-		jQuery(this).next(".prichin_text").slideToggle("fast");
-		jQuery(this).toggleClass("active");
-	});
-});
